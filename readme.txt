e6ae63e6b56c14ae87ce9a8281b00fdb775c534e
Aleksey Lashin <aleksey.lashin@gmail.com>
2014-09-21 13:01:42 +0200

    first version of cleanBrowserData extension

---
+++ readme.txt
@@ -0,0 +1,5 @@
+Icon credits:
+
+License: Creative Commons (Attribution 3.0 Unported)
+By:	Denis Sazhin
+https://www.iconfinder.com/icons/182521/cat_clean_icon#size=64
---
+++ scripts.js
@@ -0,0 +1,5 @@
+﻿chrome.browserAction.onClicked.addListener(function(activeTab)
+{
+    var newURL = "chrome://settings/clearBrowserData";
+    chrome.tabs.create({ url: newURL });
+});
